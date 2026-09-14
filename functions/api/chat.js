bd4ccf0ce978d98eaa76d959c8ebfbeb6deaf689
weiliang1230 <15971222556@163.com>
2026-09-14 12:23:47 +0800

    Update chat.js

--- functions/api/chat.js
+++ functions/api/chat.js
@@ -1,6 +1,6 @@
 export async function onRequestPost(context) {
   // 从 Cloudflare 环境变量中读取 Coze 的 Token 和 Bot ID
-  const { pat_x2ZfifQ5flKF1uusLrDM8sqkW3A6Schet4ZjkhpA4sHRUKzAdiQGu1tUxVMgKCHi, 7681199813144150035 } = context.env;
+  const { pat_natbLqQO8yNCceRSYqzVL4f8tjilVp4mr1KQqaMV0PftoOhNgZOdJ9w1lQilaSNZ, 7681199813144150035 } = context.env;
 
   try {
     const { message } = await context.request.json();
@@ -9,7 +9,7 @@ export async function onRequestPost(context) {
     const response = await fetch("https://api.coze.cn/v3/chat", {
       method: "POST",
       headers: {
-        "Authorization": `Bearer ${pat_x2ZfifQ5flKF1uusLrDM8sqkW3A6Schet4ZjkhpA4sHRUKzAdiQGu1tUxVMgKCHi}`,
+        "Authorization": `Bearer ${pat_natbLqQO8yNCceRSYqzVL4f8tjilVp4mr1KQqaMV0PftoOhNgZOdJ9w1lQilaSNZ}`,
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
